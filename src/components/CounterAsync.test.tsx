510f7646f42ce412b718675319f25557c6e819f1
MUmarOfficial <hafiz.mumer03@gmail.com>
2025-12-25 12:09:27 +0500

    new

--- src/components/CounterAsync.test.tsx
+++ src/components/CounterAsync.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, it, expect, beforeEach, vi } from "vitest";
 import CounterAsync from "./CounterAsync";
 
@@ -7,14 +7,25 @@ describe("CounterAsync component", () => {
 
     beforeEach(() => {
         mockFetchInitialCount.mockResolvedValue(10);
+        render(<CounterAsync fetchInitialCount={mockFetchInitialCount} />);
+
     });
 
     it("should render the initial value from fetchInitialCount", async () => {
-        render(<CounterAsync fetchInitialCount={mockFetchInitialCount} />);
         const loadingElement = screen.getByText("Loading...");
         expect(loadingElement).toBeInTheDocument();
         const countValueElement = await screen.findByTestId("countValue");
         expect(countValueElement.textContent).toBe("Count is 10");
+    });
+
+    it("should increment the value on increment btn click", async () => {
+        const loadingElement = screen.getByText("Loading...");
+        expect(loadingElement).toBeInTheDocument();
+        const countValueElement = await screen.findByTestId("countValue");
+        expect(countValueElement.textContent).toBe("Count is 10");
+        const incrementBtnElement = screen.getByTestId("incrementBtn")
+        fireEvent.click(incrementBtnElement);
+        expect(countValueElement.textContent).toBe("Count is 11");
     });
 });
 
